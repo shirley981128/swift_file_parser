39e0dc981f493734cfd3700ff3e07ad0f2c61f93
shirley <wenxueli@vip.qq.com>
2019-12-28 18:01:40 +0800

    Update MT940Parser.go

--- file_parser/mt940_parser/MT940Parser.go
+++ file_parser/mt940_parser/MT940Parser.go
@@ -1,7 +1,7 @@
 package mt940_parser
 
 import (
-	. "github.com/shirley981128/file_parser/common_parser"
+	. "github.com/shirley981128/swift_file_parser/file_parser/common_parser"
 	"log"
 	"strings"
 )
